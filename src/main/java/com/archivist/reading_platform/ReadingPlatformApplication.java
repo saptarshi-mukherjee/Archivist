8969339091aeb63ed386835143fb37339c8c876e
saptarshi <sapbum1234@gmail.com>
2025-04-23 10:00:46 +0530

    initial commit

---
+++ src/main/java/com/archivist/reading_platform/ReadingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.archivist.reading_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/archivist/reading_platform/ReadingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archivist.reading_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
